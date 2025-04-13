91c5d03f7fbe8bc90fb85032bd71d98d9aaf2328
nicholastcs <nictan11@gmail.com>
2025-04-13 08:21:14 +0800

    chore: update wl-paste

--- hypr/.config/hypr/start.sh
+++ hypr/.config/hypr/start.sh
@@ -3,6 +3,10 @@ hypridle &
 
 swayosd-server &
 
+wl-paste --type text --watch cliphist store &
+
+wl-paste --type image --watch cliphist store &
+
 swww-daemon &
 
 swww img ~/Wallpapers/retro_city.gif &
